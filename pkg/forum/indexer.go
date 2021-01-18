8fcf327e3bde3aec0a42a8a5ca4d87a03399633e
FairUse <usefair@outlook.com>
2021-01-18 21:02:17 +0100

    working search query with fragment highlighter

--- pkg/forum/indexer.go
+++ pkg/forum/indexer.go
@@ -13,9 +13,12 @@ type Indexer struct {
 
 func NewForumIndex() *Indexer {
 	mapping := bleve.NewIndexMapping()
-	index, err := bleve.New("example.bleve", mapping)
+	index, err := bleve.Open("example.bleve")
 	if err != nil {
-		panic(err)
+		index, err = bleve.New("example.bleve", mapping)
+		if err != nil {
+			panic(err)
+		}
 	}
 	return &Indexer{idx: index}
 }
@@ -27,7 +30,7 @@ func (f *Indexer) AddBody(id string, b Body) {
 	f.count++
 	f.batch.Index(id, b)
 	if f.batch.TotalDocsSize() > 100*1024*124 {
-		fmt.Println("flushing search index batch of size", f.batch.TotalDocsSize(), "(", f.count,"total posts seen)")
+		fmt.Println("flushing search index batch of size", f.batch.TotalDocsSize(), "(", f.count, "total posts seen)")
 		f.idx.Batch(f.batch)
 		f.batch = nil
 	}
@@ -46,5 +49,22 @@ func (f *Indexer) TestIndex(bodies []Body) {
 	for _, body := range bodies {
 		f.AddBody(body.Id, body)
 		//  f.idx.Index("test", body)
+	}
+}
+
+func (f *Indexer) Search(query string) {
+	q := bleve.NewQueryStringQuery(query)
+
+	searchRequest := bleve.NewSearchRequest(q)
+	searchRequest.Fields = []string{"*"}
+	searchRequest.Size = 100
+	searchRequest.Highlight = bleve.NewHighlight() // WithStyle("ansi")
+	searchResult, _ := f.idx.Search(searchRequest)
+
+	fmt.Println("search took", searchResult.Took)
+	fmt.Println(searchResult.Total, "documents found")
+
+	for nr, i := range searchResult.Hits {
+		fmt.Println(nr, i.Fragments)
 	}
 }
